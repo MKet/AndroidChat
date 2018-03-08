cea129b7222107cdcf6225394d1fa5c03fe80bfa
Marco Ketelaars <marco-ketelaars94@hotmail.com>
2018-03-08 15:41:15 +0100

    add messaging

--- app/src/main/java/com/world/hello/myapplication/MainActivity.java
+++ app/src/main/java/com/world/hello/myapplication/MainActivity.java
@@ -1,17 +1,26 @@
 package com.world.hello.myapplication;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.text.format.DateFormat;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseListOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,7 +50,29 @@ public class MainActivity extends AppCompatActivity {
             // Load chat room contents
             displayChatMessages();
         }
+
+        FloatingActionButton fab =
+                findViewById(R.id.fab);
+
+        fab.setOnClickListener((v) -> {
+                EditText input = findViewById(R.id.input);
+
+                // Read the input field and push a new instance
+                // of ChatMessage to the Firebase database
+                FirebaseDatabase.getInstance()
+                        .getReference()
+                        .push()
+                        .setValue(new ChatMessage(input.getText().toString(),
+                                FirebaseAuth.getInstance()
+                                        .getCurrentUser()
+                                        .getDisplayName())
+                        );
+
+                // Clear the input
+                input.setText("");
+            });
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode,
@@ -95,6 +126,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void displayChatMessages() {
+        ListView listOfMessages = findViewById(R.id.list_of_messages);
 
+        FirebaseListOptions.Builder<ChatMessage> builder = new FirebaseListOptions.Builder<>();
+        builder.setLayout(R.layout.message);
+        FirebaseListOptions<ChatMessage> build = builder.build();
+
+        FirebaseListAdapter<ChatMessage> adapter = new FirebaseListAdapter<ChatMessage>(build) {
+            @Override
+            protected void populateView(View v, ChatMessage model, int position) {
+                // Get references to the views of message.xml
+                TextView messageText = v.findViewById(R.id.message_text);
+                TextView messageUser = v.findViewById(R.id.message_user);
+                TextView messageTime = v.findViewById(R.id.message_time);
+
+                // Set their text
+                messageText.setText(model.getMessageText());
+                messageUser.setText(model.getMessageUser());
+
+                // Format the date before showing it
+                messageTime.setText(DateFormat.format("dd-MM-yyyy (HH:mm:ss)",
+                        model.getMessageTime()));
+            }
+        };
+
+        listOfMessages.setAdapter(adapter);
     }
 }
